7b2b12d9ec2967668b7f82060b5d401967362936
Luis Simon Martínez <simonmartinez.luis@gmail.com>
2025-08-06 18:34:03 -0500

    fix test

--- test/app.test.js
+++ test/app.test.js
@@ -5,6 +5,6 @@ describe('GET /', () => {
   it('should return HTML', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toContain('Bootcamp de GitHub Action en Código Facilito');
+    expect(res.text).toContain('Prueba CI/CD Luis Simon Martinez');
   });
 });
